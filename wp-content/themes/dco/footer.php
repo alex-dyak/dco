5d0c851b00c8ac748825cf8cb846da5454e5a094
alex dyakonov <alexdyakonov@3467473.no-reply.drupal.org>
2017-05-31 10:42:19 +0300

    32761 Add Wysiwyg widgets plugin. Add info email field to Theme options. Create footer.

--- wp-content/themes/dco/footer.php
+++ wp-content/themes/dco/footer.php
@@ -11,10 +11,15 @@
 
 ?>
 			<footer id="footer" class="source-org vcard copyright" role="contentinfo">
+				<!-- Mail to Button -->
+				<p><a href="mailto:<?php echo get_option( 'info_email_address' ); ?>" class="btn connect"><?php _e( 'D&CONNECT', 'dco' ); ?></a></p>
+				<!-- Return to top link -->
+				<p><a href="#top"><?php _e( 'RETURN TO TOP', 'dco' ); ?></a></p>
+				<!-- Copyright -->
 				<small>
 					<?php
 					if ( $copyright = get_option( 'w4p_copyright' ) ) {
-						echo esc_html( $copyright );
+						echo esc_html( $copyright ) . ' <a href="mailto:' . get_option( 'info_email_address' ) . '">' . get_option( 'info_email_address' ) . '</a>';
 					} else {
 						echo sprintf( esc_html__( 'Copyright © %d. %s. All Rights Reserved.', 'dco' ), date( 'Y' ), get_bloginfo( 'name' ) );
 					}
