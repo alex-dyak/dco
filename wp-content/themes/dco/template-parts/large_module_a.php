df47e8ee910fc2b5bd409f4d4118ce7ce2145bde
alex dyakonov <alexdyakonov@3467473.no-reply.drupal.org>
2017-06-02 17:45:37 +0300

    32793 Add Background and Title color. Change quote display.

--- wp-content/themes/dco/template-parts/large_module_a.php
+++ wp-content/themes/dco/template-parts/large_module_a.php
@@ -1,5 +1,33 @@
 <?php
-$image = get_sub_field( 'image' );
+$image       = get_sub_field( 'image' );
+$select      = get_sub_field( 'select_place' );
+$quote_title = get_sub_field( 'pullquote_title' );
+$quote_body  = get_sub_field( 'pullquote_body' );
+
+$background_color = get_field( 'background_color' );
+$title_color      = get_field( 'title_color' );
+
+switch ( $select ) {
+	case 'none':
+		$class_tilte = 'none';
+		$class_body  = 'none';
+		break;
+	case 'top_right':
+		$class_tilte = 'top_right';
+		$class_body  = 'top_right';
+		break;
+	case 'middle_left':
+		$class_tilte = 'middle_left';
+		$class_body  = 'middle_left';
+		break;
+	case 'bottom_right':
+		$class_tilte = 'bottom_right';
+		$class_body  = 'bottom_right';
+		break;
+	default:
+		$class_tilte = '';
+		$class_body  = '';
+}
 if ( ! empty ( $image ) ):
 	$url = $image['url'];
 	$mobile_img   = $image['sizes'][ 'mobile_img' ];
@@ -7,6 +35,12 @@ if ( ! empty ( $image ) ):
 	?>
 
 	<div class="image-large-module-a">
+		<div class="quote_title-<?php echo $class_tilte; ?>" style="color: <?php echo $title_color; ?>">
+			<?php echo $quote_title; ?>
+		</div>
+		<div class="quote_body-<?php echo $class_body; ?>" style="background: <?php echo $background_color; ?>">
+			<?php echo $quote_body; ?>
+		</div>
 		<img src="<?php echo $url; ?>" srcset="<?php echo $mobile_img; ?> 480w, <?php echo $plunshet_img; ?> 768w">
 	</div>
 
