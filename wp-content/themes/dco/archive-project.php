69d64f9ec41f1c94e998c5af4627791b508d99eb
alex dyakonov <alexdyakonov@3467473.no-reply.drupal.org>
2017-06-02 15:21:22 +0300

    32801 Create projects archive page.

--- wp-content/themes/dco/archive-project.php
+++ wp-content/themes/dco/archive-project.php
@@ -9,15 +9,30 @@
 
 get_header(); ?>
 
+<?php
+$counter = 0;
+?>
+
 <?php if ( have_posts() ) :
-	while ( have_posts() ) : the_post(); ?>
-
-
+	while ( have_posts() ) : the_post();
+		$counter ++;
+		if( $counter%2 != 0 ) {
+			$class = 'left';
+		} else {
+			$class = 'right';
+		}
+		if ( get_field( 'project_color' ) ) {
+			$project_color = get_field( 'project_color' );
+		}
+		?>
+		<?php if( get_field( 'landing_image' ) ) : ?>
+			<div class="landing-title-<?php echo $class; ?>"><?php the_title(); ?></div>
+			<div class="image-landing" style="background-color: <?php echo $project_color; ?>">
+				<img src="<?php echo get_field( 'landing_image' ); ?>"/>
+			</div>
+			<?php endif; ?>
 
 	<?php endwhile;
 endif; ?>
-
-<?php post_navigation(); ?>
-
 
 <?php get_footer(); ?>
