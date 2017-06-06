0c172b0c788bf0c17277e17801a763fdd3e5a750
Nikolay Chernay <nchernay@corp.web4pro.com.ua>
2017-06-06 17:52:11 +0300

    32802 Styling landing page

--- wp-content/themes/dco/archive-project.php
+++ wp-content/themes/dco/archive-project.php
@@ -12,29 +12,49 @@ get_header(); ?>
 <?php
 $counter = 0;
 ?>
+<div class="landing">
+	<?php if ( have_posts() ) :
+		while ( have_posts() ) : the_post();
+			$counter ++;
+			if( $counter%2 != 0 ) {
+				$class = 'left';
+			} else {
+				$class = 'right';
+			}
+			if ( get_field( 'background_color' ) ) {
+				$project_color = get_field( 'background_color' );
+			}
+			?>
 
-<?php if ( have_posts() ) :
-	while ( have_posts() ) : the_post();
-		$counter ++;
-		if( $counter%2 != 0 ) {
-			$class = 'left';
-		} else {
-			$class = 'right';
-		}
-		if ( get_field( 'background_color' ) ) {
-			$project_color = get_field( 'background_color' );
-		}
-		?>
-		<?php if( get_field( 'landing_image' ) ) : ?>
-			<a href="<?php the_permalink(); ?>">
-				<div class="landing-title-<?php echo $class; ?>"><?php the_title(); ?></div>
-				<div class="image-landing" style="background-color: <?php echo $project_color; ?>">
-					<img src="<?php echo get_field( 'landing_image' ); ?>"/>
-				</div>
-			</a>
-		<?php endif; ?>
+			<?php
+				$image = get_field( 'landing_image' );
+			if ( ! empty ( $image ) ) {
+				$url = $image['url'];
+				$full_img_mobile_small = $image['sizes']['full_img_mobile_small'];
+				$full_img_mobile_large = $image['sizes']['full_img_mobile_large'];
+				$full_img_tablet = $image['sizes']['full_img_tablet'];
+				$full_img_desktop_small = $image['sizes']['full_img_desktop_small'];
+				$full_img_desktop_large = $image['sizes']['full_img_desktop_large'];
+			}
+			?>
 
-	<?php endwhile;
-endif; ?>
+			<?php if( get_field( 'landing_image' ) ) : ?>
+				<a href="<?php the_permalink(); ?>" class="landing-block">
+					<div class="landing-title <?php echo $class; ?>"  style="background-color: <?php echo $project_color; ?>"><strong><?php the_title(); ?></strong></div>
+					<div class="landing-image">
+						<img src="<?php echo $url; ?>" srcset="
+							<?php echo $full_img_mobile_small; ?> 480w,
+							<?php echo $full_img_mobile_large; ?> 768w,
+							<?php echo $full_img_tablet; ?> 992w,
+							<?php echo $full_img_desktop_small; ?> 1200w,
+							<?php echo $full_img_desktop_large; ?> 1920w">
+					</div>
+				</a>
+			<?php endif; ?>
+
+		<?php endwhile;
+	endif; ?>
+</div>
+
 
 <?php get_footer(); ?>
