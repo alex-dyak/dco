fae010d5612cc21d15df4ccb6711e731ec6847e4
Evgenii Kolmyk <ekolmyk@corp.web4pro.com.ua>
2017-06-07 10:43:56 +0300

    32830 add aproach and approach items list blocks templates

---
+++ wp-content/themes/dco/template-parts/profile/content-approach_block.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Profile approach block template.
+ */
+?>
+<div class="approach-block">
+	<h2><?php the_sub_field( 'title' ); ?></h2>
+
+	<h3><?php the_sub_field( 'sub_title' ); ?></h3>
+
+	<?php the_sub_field( 'content' ); ?>
+
+	<div class="graph">
+		<?php $graph_image_id = get_sub_field( 'approach_graphic' ); ?>
+		<?php $graph_size = 'full'; ?>
+		<?php echo wp_get_attachment_image( $graph_image_id, $graph_size ); ?>
+	</div>
+
+	<!-- BAckground image -->
+	<?php $background = get_sub_field( 'approach_background' ); ?>
+	<?php $background_size = get_sub_field( 'approach_background' ); ?>
+	<?php echo wp_get_attachment_image( $background, $background_size ); ?>
+
+</div>
---
+++ wp-content/themes/dco/template-parts/profile/content-approach_items_list_block.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *  Approach items list block template.
+ */
+?>
+<?php if ( have_rows( 'approach_items' ) ): ?>
+	<div class="approach-items">
+
+		<?php while ( have_rows( 'approach_items' ) ) : the_row(); ?>
+
+			<h3><?php the_sub_field( 'group_title' ); ?></h3>
+
+			<?php if ( have_rows( 'items' ) ): ?>
+
+				<?php while ( have_rows( 'items' ) ) : the_row(); ?>
+
+					<p><?php the_sub_field( 'item_title' ); ?></p>
+
+				<?php endwhile; ?>
+
+			<?php endif; ?>
+
+		<?php endwhile; ?>
+
+	</div>
+<?php endif; ?>
+
+<!-- Background image -->
+<?php
+	$background_image = get_sub_field( 'background_image' );
+	$image_size       = 'full';
+?>
+<?php if ( ! empty( $background_image ) && is_int( $background_image ) ) : ?>
+	<?php echo wp_get_attachment_image( $background_image, $image_size ); ?>
+<?php endif; ?>
