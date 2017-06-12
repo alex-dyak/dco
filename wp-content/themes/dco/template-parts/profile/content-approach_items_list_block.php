cc31694098bb25b1a4240133fce80fc749e739a3
Nikolay Chernay <nchernay@corp.web4pro.com.ua>
2017-06-12 15:42:57 +0300

    32829 Approach block styled

--- wp-content/themes/dco/template-parts/profile/content-approach_items_list_block.php
+++ wp-content/themes/dco/template-parts/profile/content-approach_items_list_block.php
@@ -5,31 +5,31 @@
 ?>
 <?php if ( have_rows( 'approach_items' ) ): ?>
 	<div class="approach-items">
+		<div class="container">
+			<?php while ( have_rows( 'approach_items' ) ) : the_row(); ?>
 
-		<?php while ( have_rows( 'approach_items' ) ) : the_row(); ?>
+				<h3><?php the_sub_field( 'group_title' ); ?></h3>
 
-			<h3><?php the_sub_field( 'group_title' ); ?></h3>
+				<?php if ( have_rows( 'items' ) ): ?>
 
-			<?php if ( have_rows( 'items' ) ): ?>
+					<?php while ( have_rows( 'items' ) ) : the_row(); ?>
 
-				<?php while ( have_rows( 'items' ) ) : the_row(); ?>
+						<p><?php the_sub_field( 'item_title' ); ?></p>
 
-					<p><?php the_sub_field( 'item_title' ); ?></p>
+					<?php endwhile; ?>
 
-				<?php endwhile; ?>
+				<?php endif; ?>
 
-			<?php endif; ?>
+			<?php endwhile; ?>
+		</div>
 
-		<?php endwhile; ?>
-
+		<!-- Background image -->
+		<?php
+			$background_image = get_sub_field( 'background_image' );
+			$image_size       = 'full';
+		?>
+		<?php if ( ! empty( $background_image ) && is_int( $background_image ) ) : ?>
+			<?php echo wp_get_attachment_image( $background_image, $image_size ); ?>
+		<?php endif; ?>
 	</div>
-<?php endif; ?>
-
-<!-- Background image -->
-<?php
-	$background_image = get_sub_field( 'background_image' );
-	$image_size       = 'full';
-?>
-<?php if ( ! empty( $background_image ) && is_int( $background_image ) ) : ?>
-	<?php echo wp_get_attachment_image( $background_image, $image_size ); ?>
 <?php endif; ?>
