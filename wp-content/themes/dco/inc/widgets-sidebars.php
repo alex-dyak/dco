79d3f51577cff6f38e6f8d2710dc1c7c306e08f6
Nikolay Chernay <nchernay@corp.web4pro.com.ua>
2017-06-13 11:17:23 +0300

    32833 styling team grid

--- wp-content/themes/dco/inc/widgets-sidebars.php
+++ wp-content/themes/dco/inc/widgets-sidebars.php
@@ -356,7 +356,7 @@ class W4P_Team_Widget extends WP_Widget {
 		extract( $args );
 		$title  = apply_filters( 'widget_title', $instance['title'] ); /* The widget title. */
 		echo $before_widget;
-		if ( $title ) { echo $before_title . $title . $after_title; }
+		/*if ( $title ) { echo $before_title . $title . $after_title; }*/
 		$members = $this->get_team_members();
 		dco_locate_template( 'widgets/team', array( 'members' => $members ) );
 		echo $after_widget;
--- wp-content/themes/dco/template-parts/widgets/team.php
+++ wp-content/themes/dco/template-parts/widgets/team.php
@@ -3,46 +3,57 @@
  * Team block template.
  */
 ?>
-<?php if ( ! empty( $members ) ): ?>
-	<?php foreach ( $members as $member_id => $member ) : ?>
+<div class="team-holder container">
+	<h3 class="widget-title">Team</h3> <!--TODO Need to change this to a variable-->
+	<div class="team-block">
+		<?php if ( ! empty( $members ) ): ?>
+			<?php foreach ( $members as $member_id => $member ) : ?>
+				<div class="team-member">
+					<div class="team-member-holder">
+						<?php if ( ! empty( $member['name'] ) ): ?>
+							<span class="name">
+							<?php echo implode( ' ', $member['name'] ) ?>
+						</span>
+						<?php endif; ?>
 
-		<?php if ( ! empty( $member['name'] ) ): ?>
-			<?php echo implode( ' ', $member['name'] ) ?>
+						<?php if ( ! empty( $member['position'] ) ): ?>
+							<span class="position">
+						<?php echo $member['position']; ?>
+						</span>
+						<?php endif; ?>
+						<div class="dots"></div>
+					</div>
+
+					<?php if ( ! empty( $member['photo_preview'] ) ): ?>
+						<?php echo $member['photo_preview']; ?>
+					<?php endif; ?>
+
+					<!-- Member modal block -->
+					<div class="member-modal">
+
+						<?php if ( ! empty( $member['photo'] ) ): ?>
+							<?php echo $member['photo']; ?>
+						<?php endif; ?>
+
+						<?php if ( ! empty( $member['name'] ) ): ?>
+							<?php echo implode( ' ', $member['name'] ) ?>
+						<?php endif; ?>
+
+						<span>
+						<?php if ( ! empty( $member['position'] ) ): ?>
+							<?php echo $member['position']; ?>
+						<?php endif; ?>
+					</span>
+
+						<?php if ( ! empty( $member['description'] ) ): ?>
+							<?php echo $member['description']; ?>
+						<?php endif; ?>
+
+					</div>
+					<!-- End Member modal block -->
+
+				</div>
+			<?php endforeach; ?>
 		<?php endif; ?>
-
-		<span>
-			<?php if ( ! empty( $member['position'] ) ): ?>
-				<?php echo $member['position']; ?>
-			<?php endif; ?>
-		</span>
-
-		<?php if ( ! empty( $member['photo_preview'] ) ): ?>
-			<?php echo $member['photo_preview']; ?>
-		<?php endif; ?>
-
-		<!-- Member modal block -->
-		<div class="member-modal">
-
-			<?php if ( ! empty( $member['photo'] ) ): ?>
-				<?php echo $member['photo']; ?>
-			<?php endif; ?>
-
-			<?php if ( ! empty( $member['name'] ) ): ?>
-				<?php echo implode( ' ', $member['name'] ) ?>
-			<?php endif; ?>
-
-			<span>
-				<?php if ( ! empty( $member['position'] ) ): ?>
-					<?php echo $member['position']; ?>
-				<?php endif; ?>
-			</span>
-
-			<?php if ( ! empty( $member['description'] ) ): ?>
-				<?php echo $member['description']; ?>
-			<?php endif; ?>
-
-		</div>
-		<!-- End Member modal block -->
-
-	<?php endforeach; ?>
-<?php endif; ?>
+	</div>
+</div>
