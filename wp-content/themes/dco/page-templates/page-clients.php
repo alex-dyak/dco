64007c58da8e74bb00e39792d2327e5898ed02f3
alex dyakonov <alexdyakonov@3467473.no-reply.drupal.org>
2017-10-17 12:06:48 +0300

    34943 Create slider

--- wp-content/themes/dco/page-templates/page-clients.php
+++ wp-content/themes/dco/page-templates/page-clients.php
@@ -9,68 +9,54 @@ get_header(); ?>
 <?php if ( have_posts() ) :
 	while ( have_posts() ) : the_post(); ?>
 
-		<article class="post" id="post-<?php the_ID(); ?>">
+        <article class="post" id="post-<?php the_ID(); ?>">
 
 			<?php
+			// Slider speed value. Could be changed from admin panel.
+			$speed = get_field( 'slider_speed' ) ? get_field( 'slider_speed' )
+				: 6000;
+
 			// Display banner.
-			$banners = array();
+			$banners = [];
 			$counter = 0;
-			if ( have_rows( 'page_banner' ) ) {
-				while ( have_rows( 'page_banner' ) ) {
-					the_row();
-					$banners[ $counter ]['title'][] = get_sub_field( 'image_title' );
-					$banners[ $counter ]['image'][] = get_sub_field( 'image' );
-					$counter ++;
-				}
-			}
-			// Get random element from $banners
-			$banner = array_rand( $banners );
 
-			if ( $banners[ $banner ] ) {
-				$title = $banners[ $banner ]['title'][0];
-				$image = $banners[ $banner ]['image'][0];
-
-				if ( ! empty ( $image ) ):
-					$url = $image['url'];
-					$full_img_mobile_small   = $image['sizes']['full_img_mobile_small'];
-					$full_img_mobile_large   = $image['sizes']['full_img_mobile_large'];
-					$full_img_tablet         = $image['sizes']['full_img_tablet'];
-					$full_img_desktop_small  = $image['sizes']['full_img_desktop_small'];
-					$full_img_desktop_medium = $image['sizes']['full_img_desktop_medium'];
-					$full_img_desktop_large  = $image['sizes']['full_img_desktop_large'];
-					?>
-
-					<div class="imageClientPage">
-						<div class="imageClientPage-image js-fullHeightImage">
-							<div class="lazyload parallaxImg"
-							     data-bgset="<?php echo $full_img_mobile_small; ?> 480w,
-                                            <?php echo $full_img_mobile_large; ?> 768w,
-                                            <?php echo $full_img_tablet; ?> 992w,
-                                            <?php echo $full_img_desktop_small; ?> 1200w,
-                                            <?php echo $full_img_desktop_medium; ?> 1620w,
-                                            <?php echo $full_img_desktop_large; ?> 1920w"
-							     data-sizes="auto"></div>
-
-							<img src="<?php echo $url; ?>" srcset="
-				            <?php echo $full_img_mobile_small; ?> 480w,
-				            <?php echo $full_img_mobile_large; ?> 768w,
-				            <?php echo $full_img_tablet; ?> 992w">
-						</div>
-
-						<?php if ( $title ) : ?>
-							<div class="imageClientPage-title"><?php echo $title; ?></div>
-						<?php endif; ?>
-					</div>
-				<?php endif;
-			}
 			?>
+            <div class="profile-slider-holder">
+                <div class="profile-slider js-profile-slider"
+                     data-speed="<?php echo $speed; ?>">
+					<?php
+					if ( have_rows( 'page_banner' ) ) {
+						while ( have_rows( 'page_banner' ) ) {
+							the_row();
+
+							$title = get_sub_field( 'image_title' );
+							$image = get_sub_field( 'image' );
+							$size = 'full';
+
+							if ( ! empty ( $image ) ): ?>
+								<?php if ( ! empty( $image ) ) : ?>
+                                    <div>
+										<?php echo wp_get_attachment_image( $image['id'],
+											$size ); ?>
+										<?php if ( $title ) : ?>
+                                            <div class="imageClientPage-title"><?php echo $title; ?></div>
+										<?php endif; ?>
+                                    </div>
+								<?php endif; ?>
+							<?php endif;
+						}
+					}
+					?>
+                </div>
+            </div>
+
 
 			<?php if ( get_field( 'filter_area' ) ) : ?>
-				<div class="clientPage container">
+                <div class="clientPage container">
 					<?php echo get_field( 'filter_area' ); ?>
-				</div>
+                </div>
 			<?php endif; ?>
-		</article>
+        </article>
 
 	<?php endwhile;
 endif; ?>
