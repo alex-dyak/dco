4f59f5cbf44e880fbdf9ac7e10d8e30d731ac1dc
alex dyakonov <alexdyakonov@3467473.no-reply.drupal.org>
2017-10-17 12:46:45 +0300

    34943 Add random first slide.

--- wp-content/themes/dco/page-templates/page-clients.php
+++ wp-content/themes/dco/page-templates/page-clients.php
@@ -10,42 +10,61 @@ get_header(); ?>
 	while ( have_posts() ) : the_post(); ?>
 
 		<article class="post" id="post-<?php the_ID(); ?>">
-            <!-- W3TC_DYNAMIC_SECURITY mfunc -->
+
 			<?php
 			// Slider speed value. Could be changed from admin panel.
 			$speed = get_field( 'slider_speed' ) ? get_field( 'slider_speed' ) : 6000;
-
-			// Display banner.
-			$banners = [];
-			$counter = 0;
 			?>
-            <!--/mfunc W3TC_DYNAMIC_SECURITY -->
+
             <div class="profile-slider-holder">
-                <div class="profile-slider js-profile-slider"
-                     data-speed="<?php echo $speed; ?>">
+                <div class="profile-slider js-profile-slider" data-speed="<?php echo $speed; ?>">
+                    <!-- W3TC_DYNAMIC_SECURITY mfunc -->
 					<?php
-					if ( have_rows( 'page_banner' ) ) {
-						while ( have_rows( 'page_banner' ) ) {
-							the_row();
+					$repeater = get_field( 'page_banner' );
+					$random_rows = array_rand( $repeater, ( count( $repeater ) ) );
+					shuffle( $random_rows );
+					?>
+                    <!--/mfunc W3TC_DYNAMIC_SECURITY -->
+                    <?php
+					foreach( $random_rows as $random_row ){
 
-							$title = get_sub_field( 'image_title' );
-							$image = get_sub_field( 'image' );
-							$size = 'full';
+						$title = $repeater[$random_row]['image_title'];
+						$image = $repeater[$random_row]['image'];
+						$size = 'full';
 
-
-							if ( ! empty ( $image ) ): ?>
-								<?php if ( ! empty( $image ) ) : ?>
-                                    <div>
-										<?php echo wp_get_attachment_image( $image['id'],
-											$size ); ?>
-										<?php if ( $title ) : ?>
-                                            <div class="imageClientPage-title"><?php echo $title; ?></div>
-										<?php endif; ?>
-                                    </div>
-								<?php endif; ?>
-							<?php endif;
-						}
+						if ( ! empty ( $image ) ): ?>
+							<?php if ( ! empty( $image ) ) : ?>
+                                <div>
+									<?php echo wp_get_attachment_image( $image['id'], $size ); ?>
+									<?php if ( $title ) : ?>
+                                        <div class="imageClientPage-title"><?php echo $title; ?></div>
+									<?php endif; ?>
+                                </div>
+							<?php endif; ?>
+						<?php endif;
 					}
+
+
+//					if ( have_rows( 'page_banner' ) ) {
+//						while ( have_rows( 'page_banner' ) ) {
+//							the_row();
+//
+//							$title = get_sub_field( 'image_title' );
+//							$image = get_sub_field( 'image' );
+//							$size = 'full';
+//
+//							if ( ! empty ( $image ) ): ?>
+<!--								--><?php //if ( ! empty( $image ) ) : ?>
+<!--                                    <div>-->
+<!--										--><?php //echo wp_get_attachment_image( $image['id'], $size ); ?>
+<!--										--><?php //if ( $title ) : ?>
+<!--                                            <div class="imageClientPage-title">--><?php //echo $title; ?><!--</div>-->
+<!--										--><?php //endif; ?>
+<!--                                    </div>-->
+<!--								--><?php //endif; ?>
+<!--							--><?php //endif;
+//						}
+//					}
 					?>
                 </div>
             </div>
